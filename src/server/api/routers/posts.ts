b8cee715391dfae5ba67d998c2b94fcad94bf7f7
Darkness-2 <justinmmm690@gmail.com>
2023-03-27 21:19:29 -0400

    Create profile page with SSG helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,18 +1,10 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { z } from "zod";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "../trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 // Allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
